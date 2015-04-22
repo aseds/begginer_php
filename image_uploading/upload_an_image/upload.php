528121088ec8a9b465f2706bbca6bc3b5b1aa0bb
aseds <s.sami.ghayeni@gmail.com>
2015-04-23 03:47:38 +0430

    Update upload.php
    this is desc of this commit

--- image_uploading/upload_an_image/upload.php
+++ image_uploading/upload_an_image/upload.php
@@ -1,5 +1,6 @@
 <?php
   // this code takes the uploaded file from it's temporary place and moves it to the desired directory(/uploads)
+
   $uploads_dir = "../uploads/";
   $tmp_name = $_FILES['photo']['tmp_name'];
   $name = $_FILES['photo']['name'];
