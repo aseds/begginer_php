8d68880b98e7cdc50b31704f8cd2c7707f457f10
aseds <s.sami.ghayeni@gmail.com>
2015-04-23 03:45:38 +0430

    Create upload.php
    the file-upload-handlinkg script

---
+++ image_uploading/upload_an_image/upload.php
@@ -0,0 +1,8 @@
+<?php
+  // this code takes the uploaded file from it's temporary place and moves it to the desired directory(/uploads)
+  $uploads_dir = "../uploads/";
+  $tmp_name = $_FILES['photo']['tmp_name'];
+  $name = $_FILES['photo']['name'];
+
+  move_uploaded_file($tmp_name, "$uploads_dir/$name");
+?>
